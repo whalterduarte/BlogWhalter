dbdb8f21f5849ef2288017d0521a60837b5d4356
Whalter Duarte <dev@whalter.com.br>
2023-10-03 19:32:30 -0300

    Exibindo categorias

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const admin = require('./src/routes/admin')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 //Dot env e port
 require('dotenv').config()
 const port = process.env.PORT
 
 //Configurações
+
+    //Sessão
+    app.use(session({
+      secret: "blogwhalterd",
+      resave: true,
+      saveUninitialized: true
+    }))
+    app.use(flash())
+    //Middleware
+    app.use((req,res,next)=>{
+      res.locals.success_msg = req.flash('success_msg'),
+      res.locals.error_msg = req.flash('error_msg'),
+      next()
+    })
     //Body Parser
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(bodyParser.json())
--- src/controller/admin/homeController.js
+++ src/controller/admin/homeController.js
@@ -3,13 +3,22 @@ const mongoose = require('mongoose')
 require('../../models/Categorias')
 const Categoria = mongoose.model('categorias')
 
+
+
 exports.home = (req,res)=>{
  res.render('admin/dashboard', { layout: 'admin-layout' })
 }
 
 
 exports.categorias = (req,res)=>{
- res.render('admin/categorias', { layout: 'admin-layout' })
+  Categoria.find().lean().then((categorias)=>{
+    res.render('admin/categorias', { layout: 'admin-layout', categorias: categorias })
+  }).catch((error)=>{
+    req.flash('error_msg', 'Ouve um erro ao listar categorias')
+    res.redirect('/admin')
+  })
+ 
+
 }
 
 
@@ -33,14 +42,36 @@ exports.adicionar = (req,res)=>{
  
 
  exports.adcCategoria = (req,res)=>{
-      const novaCategoria = {
-      nome: req.body.nome,
-      slug: req.body.slug
+
+      var erros = []
+
+      if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null  ){
+          erros.push({texto: "Nome inválido"})
       }
-      new Categoria(novaCategoria).save().then(()=>{
-        console.log('Categoria salva')
-      }).catch((error)=>{
-        console.log('Erro ao salvar categoria')
-      })
- }
+
+      if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null ){
+          erros.push({texto: "Slug inválido"})
+      }
+      if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria muito pequeno"})
+      }
+      if (erros.length > 0) {
+        res.render('admin/addcategorias', { layout: 'admin-layout', erros: erros });
+      }else{
+        const novaCategoria = {
+          nome: req.body.nome,
+          slug: req.body.slug
+          }
+          new Categoria(novaCategoria).save().then(()=>{
+            req.flash('success_msg', 'Categoria criada com sucesso!')
+            res.redirect('../')
+          }).catch((error)=>{
+            req.flash('error_msg', 'Erro ao criar categoria, tente novamente !')
+            res.redirect('admin/categorias')
+          })
+     }
+      }
+
+
+ 
 
